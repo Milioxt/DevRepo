734cc7cfae30683db94b51d1feb80ef71dbc8282
Milioxt <emilio-port-s@hotmail.com>
2025-02-25 10:55:04 -0700

    Added main for the game class #CZACKZTANCZACKZTAN

--- games/Game.py
+++ games/Game.py
@@ -1,11 +1,8 @@
-#AND I WONDER
-#IF YOU KNOW
-#WHAT IT MEANS
-#TO FIND YOUR DREAMS
+#SHE TOOK MY MONEY WELL I'M IN NEED YEAH SHE'S A TRIFLING FRIEND INDEED OH SHE'S A GOLD DIGGER WAY OVER TOWN THAT DIGS ON ME
+#I AINT SAYING SHE A GOLD DIGGER BUT SHE AINT MESSING WITH NO BROKE NIGGAS
 
 
-
-
+from random import choice
 from Team import Team
 from Sport import Sport
 from Athlete import Athlete
@@ -33,8 +30,30 @@ class Game:
 
     def play(self):
         '''Juego simulado entre equipos'''
-        for s in self.sports_dict.values():
-            print(s)
+        league = self.A.sport.league
+        points = self.sports_dict[league]
+        a = choice(points)
+        b = choice(points)
+        self.score[self.A.name] = a
+        self.score[self.B.name] = b
+
+    def __str__(self)-> str:
+        '''Metodo para mostrar clase como string'''
+        return f"""
+        -----------------------------------------
+        Game: 
+        {self.A.name:20s}: {self.score[self.A.name]:3d} 
+        {self.B.name:20s}: {self.score[self.B.name]:3d}
+        ------------------------------------------
+        """
+        
+    def __repr__(self)-> str:
+        '''Metodo para representar clase como string'''
+        return f"Game(A={repr(self.A)}, B={repr(self.B)})"
+    
+    def to_json(self)-> dict:
+        '''Convertir Game a JSON'''
+        return {"A":self.A.to_json(), "B":self.B.to_json(), "score":self.score}
 
 
 if __name__ == "__main__":
@@ -42,8 +61,14 @@ if __name__ == "__main__":
     cz = ['Bjovik','Czack','Pfeizer','Leonard','Kempfe']
     player_a = [Athlete(x) for x in dt]
     player_b = [Athlete(x) for x in cz]
-    basketball= Sport("NBA",5,"DreamTeam")
+    basketball= Sport("DreamTeam",5,"NBA")
     t = Team("DreamTeam",basketball)
     c = Team("CzackTeam",basketball)
     game = Game(t,c)
+    print(game)
     game.play()
+    print(game)
+    print(repr(game))
+    print("---------")
+    print(game.to_json())
+    
